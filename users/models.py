d7a33d23276b208ae767976f4890a0bafc284db8
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-14 19:22:40 -0300

    fix: changing user model inheritance

--- users/models.py
+++ users/models.py
@@ -1,20 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 import uuid
 
 from users.utils import CustomUserManager
 
 
-class User(models.Model):
+class User(AbstractUser):
     username = None
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=256, null=False)
     email = models.EmailField(max_length=256, null=False, unique=True)
 
     techs = models.ManyToManyField(
-        "techs.Tech", on_delete=models.CASCADE, related_name="users"
+        "techs.Tech", related_name="users"
     )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["name"]
 
     objects = CustomUserManager()
+
