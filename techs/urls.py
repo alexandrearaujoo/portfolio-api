dd673d6690a3932c9458f43719ef0a937a942947
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-14 19:03:31 -0300

    feat: tech urls done

---
+++ techs/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("techs/", views.ListTechsView.as_view()),
+    path("techs/<pk>", views.UpdateDestroyView.as_view())
+]
