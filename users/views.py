50b114c5a303c1147e17005a532be61345309608
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-14 19:07:20 -0300

    feat: login view done

--- users/views.py
+++ users/views.py
@@ -1,10 +1,17 @@
 from rest_framework import generics
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView, Response, status
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserLoginSerializer
+
+class ListUsersView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-class ListCreateUserView(generics.ListCreateAPIView):
+class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -12,3 +19,24 @@ class ListCreateUserView(generics.ListCreateAPIView):
 class RetriveUpdateDestroyUserView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class LoginAccountView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        account = authenticate(
+            username=serializer.validated_data["username"],
+            password=serializer.validated_data["password"],
+        )
+
+        if account:
+            token, _ = Token.objects.get_or_create(user=account)
+
+            return Response({"token": token.key})
+
+        return Response(
+            {"message": "Invalid credentials"}, status.HTTP_401_UNAUTHORIZED
+        )
