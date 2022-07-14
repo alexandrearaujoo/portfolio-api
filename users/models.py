fa1c13654dcaab57c53946fd2edbfe5c09a74961
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-14 18:12:20 -0300

    feat: user model done

--- users/models.py
+++ users/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+import uuid
+
+from users.utils import CustomUserManager
 
 class User(models.Model):
     username = None
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=256, null=False)
-    email = models.EmailField(max_length=256, null=False)
+    email = models.EmailField(max_length=256, null=False, unique=True)
 
     techs = models.ManyToManyField('techs.Tech', on_delete=models.CASCADE, related_name='users')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
