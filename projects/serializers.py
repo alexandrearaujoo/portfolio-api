d78ad8add2a70e23cc1619e53504aaaa48266d7d
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-14 18:47:51 -0300

    feat: project serializer done

---
+++ projects/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+
+from .models import Project
+
+from users.models import User
+from users.serializers import UserSerializer
+
+class ProjectSerializer (serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
+    class Meta:
+        model = Project
+        fields = "__all__"
+        read_only_fields = ['id']
+        extra_kwargs = {
+            'email': {
+                'validators':
+                [UniqueValidator(queryset=User.objects.all(), message='Email already exists')]
+            },
+            'password': {'write_only': True}
+        }
