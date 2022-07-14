69b9a60c8f80ceb2c84958cf2d20939ab4bea8df
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-14 18:37:33 -0300

    feat: project model done

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Project (models.Model):
+     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+     title = models.CharField(max_length=126, null=False)
+     type = models.CharField(max_length=126, null=False)
+     slug = models.CharField(max_length=126, null=True),
+     img = models.ImageField(upload_to='projects/',null=True, blank=True)
+     link_website = models.CharField(max_length=126, null=False)
+     link_repository = models.CharField(max_length=256, null=False)
+     description = models.CharField(max_length=256, null=True)
+
+     owner = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='projects')
