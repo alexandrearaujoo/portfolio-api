2cec3b723a180a6c4a18608bd498e885d6b9d6c4
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-14 18:52:17 -0300

    feat: user views done

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+class ListCreateUserView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class RetriveUpdateDestroyUserView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
