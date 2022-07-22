2ed62332dfdb111a82677409220163d0c1234871
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-22 13:27:58 -0300

    fix: removing techs in fields array

--- users/serializers.py
+++ users/serializers.py
@@ -6,7 +6,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "name", "email", "password", "techs"]
+        fields = ["id", "name", "email", "password"]
         read_only_fields = ["id"]
         extra_kwargs = {"password": {"write_only": True}}
 
